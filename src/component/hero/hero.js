ebc4916af6afbdc10149961d20de246249fcc4f6
ayualdilasalma <ayu.aldilas@yahoo.com>
2021-06-24 14:43:14 +0700

    cleaning up layout for homepage

--- src/component/hero/hero.js
+++ src/component/hero/hero.js
@@ -1,18 +1,10 @@
 import React from "react";
 
-const Hero = () => {
+const Hero = (props) => {
   return (
     <div>
       <div className="h-screen flex flex-col justify-center items-center">
-        <h1 className="lg:text-9xl md:text-7xl text-5xl font-black text-green-600">
-          Kulkasku
-        </h1>
-        <h2 className="lg:text-6xl md:text-4xl text-2xl font-black text-gray-500 mb-14">
-          Your kitchen companion
-        </h2>
-        <button class="py-4 px-8 rounded-full shadow-md text-gray-700 text-3xl bg-yellow-500 hover:bg-yellow-600 items-center">
-          <text>Order Now</text>
-        </button>
+        {props.content}
       </div>
     </div>
   );
--- src/container/home/home.js
+++ src/container/home/home.js
@@ -2,11 +2,14 @@ import React from "react";
 
 import Hero from "../../component/hero/hero";
 import Navbar from "../../component/navbar/navbar";
+import Content from "../../component/Content/Content";
+import HomepageContent from "../../component/Homepage/Homepage";
 
 const Home = () => (
   <div className="bg-yellow-50">
     <Navbar />
-    <Hero />
+    <Hero content={<HomepageContent />} />
+    <Hero content={<Content />} />
   </div>
 );
 
